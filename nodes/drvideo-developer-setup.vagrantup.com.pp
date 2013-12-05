94952a14e4eba259485a4a872233c7a151802ea9
Johan Sydseter <johan@aptoma.com>
2013-12-05 10:35:53 +0100

    Add vhost for apache, add nodejs and grunt to the env

--- nodes/drvideo-developer-setup.vagrantup.com.pp
+++ nodes/drvideo-developer-setup.vagrantup.com.pp
@@ -5,12 +5,27 @@
 node 'drvideo-developer-setup.vagrantup.com' {
 
 
-  class { 'apache': }
+  class { 'apache': } ->
+  file { '/var/www/videoplaza-html5':
+     ensure => 'link',
+     target => '/home/vagrant/src/videoplaza-html5',
+  } 
+
 
   apache::vhost { 'videoplaza-html5':
     docroot  => '/var/www/videoplaza-html5',
   }
 
+
+
   class { 'php': }
 
+  class { 'nodejs': 
+    version => 'stable',
+  } -> 
+
+  package { 'grunt-cli':
+    provider        => 'npm',
+    install_options => ['-g']
+  }
 }
